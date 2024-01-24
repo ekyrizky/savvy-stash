78055e2c689a590369273851991b458dafa315df
ekyrizky <m.rizkyananda57@gmail.com>
2024-01-24 16:10:08 +0700

    upda typography

---
+++ lib/app/theme/typography.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTypography {
+  static TextStyle titleLarge = GoogleFonts.grapeNuts(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle titleMedium = GoogleFonts.grapeNuts(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle titleSmall = GoogleFonts.grapeNuts(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle bodyLarge = GoogleFonts.grapeNuts(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle bodyMedium = GoogleFonts.grapeNuts(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle bodySmall = GoogleFonts.grapeNuts(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle labelLarge = GoogleFonts.grapeNuts(
+    fontSize: 16,
+    fontWeight: FontWeight.w200,
+  );
+
+  static TextStyle labelMedium = GoogleFonts.grapeNuts(
+    fontSize: 14,
+    fontWeight: FontWeight.w200,
+  );
+
+  static TextStyle labelsMALL = GoogleFonts.grapeNuts(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+  );
+}
