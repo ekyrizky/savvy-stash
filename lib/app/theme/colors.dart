403b194c0e697842ccd4d6246d5b3f62c841677c
ekyrizky <m.rizkyananda57@gmail.com>
2024-01-24 13:06:54 +0700

    add theme: colors & typography

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color kPrimaryColor = Color.fromRGBO(33, 65, 96, 1);
+  static const Color kSecondaryColor = Color.fromRGBO(211, 193, 163, 1);
+  static const Color kBackgroundColor = Color.fromRGBO(244, 244, 239, 1);
+  static const Color kGreenColor = Color.fromRGBO(139, 195, 74, 1);
+  static const Color kRedColor = Color.fromRGBO(255, 78, 67, 1);
+  static const Color kGrayColor = Color.fromRGBO(170, 169, 176, 1);
+}
---
+++ lib/app/theme/text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle titleLarge = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle titleMedium = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle titleSmall = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle bodyLarge = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle bodyMedium = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle bodySmall = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle labelLarge = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w200,
+  );
+
+  static TextStyle labelMedium = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w200,
+  );
+
+  static TextStyle labelsMALL = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200,
+  );
+}
